012cb4e1092abca9212236f39673b7452119940b
Marco Buono <marco.buono@linksmt.it>
2024-09-10 15:02:25 +0200

    Minor fix

--- client/app/src/shared/partials/tip-files-whistleblower/tip-files-whistleblower.component.ts
+++ client/app/src/shared/partials/tip-files-whistleblower/tip-files-whistleblower.component.ts
@@ -17,7 +17,7 @@ export class TipFilesWhistleblowerComponent {
   @Input() fileUploadUrl: string;
   collapsed = false;
 
-  constructor(private appDataService: AppDataService, private cryptoService: CryptoService, private httpService: HttpService, protected authenticationService: AuthenticationService, protected utilsService: UtilsService, protected wbTipService: WbtipService, protected preferenceResolver:PreferenceResolver) {
+  constructor(private appDataService: AppDataService, private cryptoService: CryptoService, private httpService: HttpService, protected authenticationService: AuthenticationService, protected utilsService: UtilsService, protected wbTipService: WbtipService) {
   }
 
   downloadWBFile(wbFile: WbFile) {
