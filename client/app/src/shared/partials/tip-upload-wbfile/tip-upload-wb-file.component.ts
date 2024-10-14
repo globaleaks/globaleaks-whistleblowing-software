fe8b29f2c173dab0fdfff028d178bf9307029b2b
Anna Bellifemine <anna.bellifemine@linksmt.it>
2024-10-14 18:29:58 +0200

    fix - upload files to oe

--- client/app/src/shared/partials/tip-upload-wbfile/tip-upload-wb-file.component.ts
+++ client/app/src/shared/partials/tip-upload-wbfile/tip-upload-wb-file.component.ts
@@ -35,6 +35,7 @@ export class TipUploadWbFileComponent{
 
     if(this.key === 'oe')
       return this.tip.rfiles
+                .filter(file => file.visibility === this.key)
                 .filter(file => this.organizations.map(_=>_.files).flat().some(i => i?.id === file.id))
                 .sort((a, b) => new Date(a.creation_date).getTime() - new Date(b.creation_date).getTime());
     else 
@@ -77,20 +78,22 @@ export class TipUploadWbFileComponent{
 
       const flowJsInstance = this.utilsService.flowDefault;
 
+      let tids = this.organizations.map(_=> _.tid);
+
       flowJsInstance.opts.target = "api/recipient/rtips/" + this.tip.id + "/rfiles";
       flowJsInstance.opts.singleFile = true;
-      flowJsInstance.opts.query = {description: this.file_upload_description, visibility: this.key, fileSizeLimit: this.appDataService.public.node.maximum_filesize * 1024 * 1024, tids: this.organizations},
+      flowJsInstance.opts.query = {description: this.file_upload_description, visibility: this.key, fileSizeLimit: this.appDataService.public.node.maximum_filesize * 1024 * 1024, tids: "["+tids.toString()+"]"},
       flowJsInstance.opts.headers = {"X-Session": this.authenticationService.session.id};
       flowJsInstance.on("fileSuccess", (_) => {
-        this.recentFile.isLoading = false;
+        this.recentFile.isLoading = false   
         this.dataToParent.emit();
         this.errorFile = null;
       });
       flowJsInstance.on("fileError", (file, _) => {
-          const index = this.tip.rfiles.indexOf(this.recentFile);
-          if (index > -1) {
-            this.tip.rfiles.splice(index, 1);
-          }
+        const index = this.tip.rfiles.indexOf(this.recentFile);
+        if (index > -1) {
+          this.tip.rfiles.splice(index, 1);
+        }
         this.showError = true;
         this.errorFile = file;
         if (this.uploaderInput) {
