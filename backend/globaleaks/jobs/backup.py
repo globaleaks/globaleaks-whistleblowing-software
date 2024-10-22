71965fa856cc8b198e57dbcbd65f5f414f17a04b
Alessio Franceschini <alessio.franceschini@linksmt.it>
2024-10-22 18:11:31 +0200

    Edited transaction's management in backup's job

--- backend/globaleaks/jobs/backup.py
+++ backend/globaleaks/jobs/backup.py
@@ -3,7 +3,6 @@ import os
 import sqlite3
 import subprocess
 
-from twisted.internet.defer import inlineCallbacks
 
 from globaleaks.utils.backup import get_backups_parameter, reset_audit_log_file
 from globaleaks import models
@@ -11,36 +10,34 @@ from globaleaks.orm import db_log, transact
 from globaleaks.utils.utility import datetime_now
 from globaleaks.jobs.job import LoopingJob
 from globaleaks.settings import Settings
+from globaleaks.utils.log import log
+
+from twisted.internet.defer import inlineCallbacks
 
 
 __all__ = ['Backup']
 
 
-def run_rsync(source_path, destination_path):
+def run_rsync(session, source_path, destination_path):
     try:
         rsync_command = ["rsync", "-av", source_path, destination_path]
         subprocess.run(rsync_command, check=True)
     except Exception as e:
-        db_backup_log(e)
+        db_backup_log(session, e)
 
 
-def backup_sqlite_database_and_attachments(backup_db_path, backup_attachments_path):
-    run_rsync(Settings.attachments_path, backup_attachments_path)
+def backup_sqlite_database_and_attachments(session, backup_db_path, backup_attachments_path):
+    run_rsync(session, Settings.attachments_path, backup_attachments_path)
     source_conn = sqlite3.connect(Settings.db_file_path)
     dest_conn = sqlite3.connect(backup_db_path)
     source_conn.execute('BEGIN TRANSACTION EXCLUSIVE;')
     source_conn.backup(dest_conn)
-    run_rsync(Settings.attachments_path, backup_attachments_path)
+    run_rsync(session, Settings.attachments_path, backup_attachments_path)
     source_conn.commit()
     source_conn.close()
     dest_conn.close()
 
 
-@transact
-def wrap_get_backups_parameter(session):
-    return get_backups_parameter(session)
-
-@transact
 def get_last_backup_log(session):
     last_backup = session.query(models.AuditLog).filter(models.AuditLog.type == 'backup', models.AuditLog.data == 'OK') \
         .order_by(models.AuditLog.date.desc()) \
@@ -49,7 +46,6 @@ def get_last_backup_log(session):
     return last_backup
 
 
-@transact
 def db_backup_log(session, exception):
     if exception:
         result = 'KO: ' + str(exception)
@@ -58,9 +54,10 @@ def db_backup_log(session, exception):
         db_log(session, tid=1, type='backup', user_id='system', data='OK')
 
 
-@inlineCallbacks
-def do_backup():
-    backup_enable, backup_time_iso_8601, backup_destination_path = yield wrap_get_backups_parameter()
+@transact
+def do_backup(session):
+    backup_enable, backup_time_iso_8601, backup_destination_path = get_backups_parameter(
+        session)
 
     if not backup_enable or not backup_time_iso_8601 or not backup_destination_path:
         return
@@ -71,33 +68,35 @@ def do_backup():
         if datetime_now().time() <= backup_time_iso_8601_object:
             return
     except Exception as e:
-        db_backup_log(e)
+        db_backup_log(session, e)
         return
 
     if not os.path.exists(backup_destination_path):
         os.mkdir(backup_destination_path)
 
-    last_backup_log = yield get_last_backup_log()
+    last_backup_log = get_last_backup_log(session)
 
     if not last_backup_log or last_backup_log.date + timedelta(days=1) <= datetime_now():
         try:
-            backup_sqlite_database_and_attachments(os.path.join(
+            backup_sqlite_database_and_attachments(session, os.path.join(
                                                    backup_destination_path, 'globaleaks' + datetime_now().strftime('%Y-%m-%d_%H:%M') + '.db'),
                                                    backup_destination_path)
         except Exception as e:
-            db_backup_log(e)
+            db_backup_log(session, e)
             return
         finally:
             reset_audit_log_file(backup_destination_path)
-            db_backup_log(None)
+            db_backup_log(session, None)
 
 
 class Backup(LoopingJob):
-    interval = 300
+    interval = 60
     monitor_interval = 600
 
     def operation(self):
         """
         This function executes backup's operations
         """
+        log.info("START BACKUP JOB")
         do_backup()
+        log.info("END BACKUP JOB")
