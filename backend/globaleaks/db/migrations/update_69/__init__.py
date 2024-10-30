7756b8c12aa8690bc9bed59f2366e648c17b7193
Emanuele Bosu <emanuele.bosu@linksmt.it>
2024-10-30 15:57:15 +0100

    Update __init__.py

--- backend/globaleaks/db/migrations/update_69/__init__.py
+++ backend/globaleaks/db/migrations/update_69/__init__.py
@@ -263,7 +263,7 @@ class MigrationScript(MigrationBase):
         template_mail_config.tid = 1
         template_mail_config.lang = 'en'
         template_mail_config.var_name = 'sign_up_external_organization_info_mail_template'
-        template_mail_config.value = 'Dear {RecipientName},\n\nYour accreditation request is in status: {AccreditationStatus}\n\nKind regards,\n\n{NodeName}'
+        template_mail_config.value = 'Dear {RecipientName},\n\nYour accreditation request is in status: {AccreditationStatus}\n\n{AccreditationMotivationText}\n\nKind regards,\n\n{NodeName}'
         template_mail_config.update_date = datetime_now()
         self.session_new.add(template_mail_config)
         self.entries_count['ConfigL10N'] += 1
