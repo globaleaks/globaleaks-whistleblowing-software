9913112cd4783bfd257c11ff890000ac9b96d461
Emanuele Bosu <emanuele.bosu@linksmt.it>
2024-11-13 19:02:07 +0100

    Update test_rfile.py

--- backend/globaleaks/tests/handlers/recipient/test_rfile.py
+++ backend/globaleaks/tests/handlers/recipient/test_rfile.py
@@ -14,7 +14,7 @@ class TestWBFileWorkFlow(helpers.TestHandlerWithPopulatedDB):
 
     @inlineCallbacks
     def test_get(self):
-        yield self.perform_full_submission_actions()
+        # yield self.perform_full_submission_actions()
 
         self._handler = rtip.ReceiverFileUpload
         rtips_desc = yield self.get_rtips()
@@ -23,7 +23,7 @@ class TestWBFileWorkFlow(helpers.TestHandlerWithPopulatedDB):
             yield handler.post(rtip_desc['id'])
 
         yield Delivery().run()
-
+        
         self._handler = wbtip.ReceiverFileDownload
         wbtips_desc = yield self.get_wbtips()
         for wbtip_desc in wbtips_desc:
