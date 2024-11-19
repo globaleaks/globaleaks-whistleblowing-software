11f847397be518c8fe97cae909eb3f5972f89b6b
Anna Bellifemine <anna.bellifemine@linksmt.it>
2024-11-19 15:52:15 +0100

    cypress - test for instructor request to eo

--- client/cypress/e2e/24-test-external-organization.cy.ts
+++ client/cypress/e2e/24-test-external-organization.cy.ts
@@ -110,4 +110,38 @@ describe('add, configure, delete external organization', () => {
     });     
   });
 
+
+it("Instructor request for accreditation", function () {
+  cy.login_receiver();
+  cy.waitForUrl("/#/recipient/home");
+  cy.visit("/#/recipient/reports");
+
+  cy.get("#tip-1").should('be.visible').first().click();
+
+  cy.get('#tip-action-send-tip').invoke('click');
+
+  cy.get('#add-organization').click();
+
+  cy.get('#organization_name').type("EO requested by recipient");
+  cy.get("#pec").type("examplepec3@examplepec3.com");
+  cy.get("#confirmPec").type("examplepec3@examplepec3.com");
+  cy.get("#institutionalWebsite").type("httop://insitutionalwebsite.com");
+  cy.get("#accreditationReason").type("the motivation of my accreditation request");
+
+  cy.get("#submitButton").should("be.enabled");
+  cy.get("#submitButton").click();
+
+  cy.wait(2000);
+
+  cy.logout();
+
+  cy.login_accreditor();
+  cy.visit("/#/accreditor/organizations");
+  cy.get("#req-0").first().click();
+
+  cy.get("#accept-button").click();
+
+  cy.get(".btn-danger").should("not.exist");
+});
+
 })
