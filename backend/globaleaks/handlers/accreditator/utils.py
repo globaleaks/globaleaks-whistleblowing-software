1530d4d1eb9181a41fd052997c78c89d78d04af7
Emanuele Bosu <emanuele.bosu@linksmt.it>
2024-12-04 18:06:07 +0100

    Update utils.py

--- backend/globaleaks/handlers/accreditator/utils.py
+++ backend/globaleaks/handlers/accreditator/utils.py
@@ -72,7 +72,7 @@ def count_user_tip(session, accreditation_item: Subscriber):
             InternalTipForwarding.state,
             func.count(distinct(InternalTipForwarding.id))
         )
-        .filter(InternalTipForwarding.forwarding_internaltip_id == accreditation_item.tid)
+        .filter(InternalTipForwarding.tid == accreditation_item.tid)
         .group_by(InternalTipForwarding.state)
         .all()
     )
