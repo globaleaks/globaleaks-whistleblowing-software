29129f5fdd9fae0684725afd84dcf573ca54970b
Anna Bellifemine <anna.bellifemine@linksmt.it>
2024-12-06 17:15:15 +0100

    fix redirect home page

--- client/app/src/services/root/app-config.service.ts
+++ client/app/src/services/root/app-config.service.ts
@@ -145,11 +145,13 @@ export class AppConfigService {
         location.replace("/#/wizard");
       } 
       else if (this.router.url === "/" && !this.appDataService.public.node.root_tenant && this.appDataService.public.node.mode === 'accreditation') {
+        debugger
         location.replace("/#/login")
       }
       else if ((this.router.url === "/" || this.router.url === "/submission") && !this.appDataService.public.node.enable_signup && this.appDataService.public.node.adminonly && !this.authenticationService.session) {
         location.replace("/#/admin/home");
-      } else if (this.router.url === "/" && this.appDataService.public.node.enable_signup && !location.href.endsWith("admin/home")) {
+      } else if (this.router.url === "/" && this.appDataService.public.node.enable_signup && !location.href.endsWith("admin/home") && this.appDataService.public.node.mode !== 'accreditation') {
+        debugger
         location.replace("/#/signup");
       } else if (this.router.url === "/signup" && !this.appDataService.public.node.enable_signup) {
         location.replace("/#/");
