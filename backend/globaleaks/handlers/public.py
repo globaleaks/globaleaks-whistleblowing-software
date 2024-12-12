d4a14d7152f6a38831cb3e43e80fa069596f54dc
Emanuele Bosu <emanuele.bosu@linksmt.it>
2024-12-12 16:07:56 +0100

    Update public.py

--- backend/globaleaks/handlers/public.py
+++ backend/globaleaks/handlers/public.py
@@ -613,7 +613,6 @@ class PublicResource(BaseHandler):
     Handler responsible of serving the public API
     """
     check_roles = 'any'
-    cache_resource = True
 
     def get(self):
         """
