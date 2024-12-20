09c79d021f3c67413990bd6f29218ad548612d8f
Anna Bellifemine <anna.bellifemine@linksmt.it>
2024-12-20 12:56:16 +0100

    onboarding redirection for unatuthorized accreditation

--- client/app/src/services/root/app-interceptor.service.ts
+++ client/app/src/services/root/app-interceptor.service.ts
@@ -53,12 +53,6 @@ export class appInterceptor implements HttpInterceptor {
     }
   }
 
-  private checkRegexProtectedUrl(url: string){
-
-    let arrayContainsString = regexProtectedUrl.some(item => url.match(item));
-    return arrayContainsString;
-
-  }
 
   intercept(httpRequest: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
@@ -82,7 +76,7 @@ export class appInterceptor implements HttpInterceptor {
 
 
     if (httpRequest.url.includes("api/signup") || httpRequest.url.endsWith("api/auth/receiptauth") && !this.authenticationService.session || protectedUrls.includes(httpRequest.url)
-      || this.checkRegexProtectedUrl(httpRequest.url)) {
+      || this.authenticationService.checkRegexProtectedUrl(httpRequest.url)) {
       return this.httpClient.post("api/auth/token", {}).pipe(
         switchMap((response) =>
           from(this.cryptoService.proofOfWork(Object.assign(new TokenResponse(), response).id)).pipe(
@@ -117,6 +111,10 @@ export class ErrorCatchingInterceptor implements HttpInterceptor {
             if((request.url === "api/auth/authentication/external" || request.url === "api/user/reset/password/external") && error.status === 401 && error.error["error_code"] == undefined && this.appDataService.public.proxy_idp_enabled){
               window.location.href="/login-external-organizazion/"+this.appDataService.public.node.uuid
             }  
+
+            if((request.url === "api/accreditation/request" || this.authenticationService.checkRegexProtectedUrl(request.url)) && error.status === 401 && this.appDataService.public.proxy_idp_enabled){
+              window.location.href="/onboarding"
+            } 
 
             if (error.error["error_code"] === 10) {
               this.authenticationService.deleteSession();
