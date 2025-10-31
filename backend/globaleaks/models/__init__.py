e5b1951907d68bd0532483dd72aad2c0b180ee92
عبدالمنان <msmannan00@protonmail.com>
2025-10-31 17:39:22 +0500

    Update __init__.py

--- backend/globaleaks/models/__init__.py
+++ backend/globaleaks/models/__init__.py
@@ -1183,7 +1183,7 @@ class _User(Model):
     pgp_key_fingerprint = Column(UnicodeText, default='', nullable=False)
     pgp_key_public = Column(UnicodeText, default='', nullable=False)
     pgp_key_expiration = Column(DateTime, default=datetime_null, nullable=False)
-
+    last_expiration_reminder_date = Column(DateTime, default=datetime_null, nullable=False)
     accepted_privacy_policy = Column(DateTime, default=datetime_null, nullable=False)
     clicked_recovery_key = Column(Boolean, default=False, nullable=False)
 
