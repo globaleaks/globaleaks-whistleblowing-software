4829590af4fc65118c45eb1d5fa27061414ec1c9
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2026-05-19 11:17:50 +0200

    Ensure .github/workflows/scripts/run_tests.sh uses pinned dependencies

--- scripts/pin_backend_requirements.sh
+++ scripts/pin_backend_requirements.sh
@@ -6,7 +6,7 @@ REQ_DIR="$(dirname "${BASH_SOURCE[0]}")/../backend/requirements"
 for in_file in "${REQ_DIR}"/requirements.in.*; do
   version="${in_file##*.}"
   echo "Compiling requirements.in.${version} -> requirements.txt.${version}"
-  pip-compile --strip-extras --no-header --generate-hashes \
+  pip-compile --strip-extras --no-header --generate-hashes --allow-unsafe \
     --output-file="${REQ_DIR}/requirements.txt.${version}" \
     "${in_file}"
 done
