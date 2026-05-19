5a93366473092cc87f0b182f6c94d540b0a64da6
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2026-05-19 10:56:42 +0200

    Implement script to pin backend dependencies by hash

---
+++ scripts/pin_backend_requirements.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+REQ_DIR="$(dirname "${BASH_SOURCE[0]}")/../backend/requirements"
+
+for in_file in "${REQ_DIR}"/requirements.in.*; do
+  version="${in_file##*.}"
+  echo "Compiling requirements.in.${version} -> requirements.txt.${version}"
+  pip-compile --strip-extras --no-header --generate-hashes \
+    --output-file="${REQ_DIR}/requirements.txt.${version}" \
+    "${in_file}"
+done
