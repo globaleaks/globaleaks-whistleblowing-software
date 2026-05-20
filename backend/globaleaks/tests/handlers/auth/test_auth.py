730e3f66dcd5effed870a7d3af24828e22134376
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2026-05-20 12:59:18 +0200

    Deprecate usage of Clear-Site-Data header preferring clientside cleaning

--- backend/globaleaks/tests/handlers/auth/test_auth.py
+++ backend/globaleaks/tests/handlers/auth/test_auth.py
@@ -363,9 +363,6 @@ class TestSessionHandler(helpers.TestHandlerWithPopulatedDB):
         handler = self.request({}, headers={'x-session': session_id})
         yield handler.delete()
 
-        self.assertEqual(handler.request.responseHeaders.hasHeader("Clear-Site-Data"), True)
-        self.assertEqual(handler.request.responseHeaders.getRawHeaders("Clear-Site-Data")[0], '"*"')
-
     @inlineCallbacks
     def test_successful_admin_logout_on_management_session_prevent_clearing_site_data(self):
         self._handler = auth.AuthenticationHandler
@@ -392,9 +389,6 @@ class TestSessionHandler(helpers.TestHandlerWithPopulatedDB):
         handler = self.request({}, headers={'x-session': session_id})
         yield handler.delete()
 
-        # Verify the Clear-Site-Data header is not injected on management sessions
-        self.assertEqual(handler.request.responseHeaders.hasHeader("Clear-Site-Data"), False)
-
     @inlineCallbacks
     def test_successful_whistleblower_logout(self):
         self._handler = auth.ReceiptAuthHandler
@@ -416,9 +410,6 @@ class TestSessionHandler(helpers.TestHandlerWithPopulatedDB):
         # Logout
         handler = self.request({}, headers={'x-session': response['id']})
         yield handler.delete()
-
-        self.assertEqual(handler.request.responseHeaders.hasHeader("Clear-Site-Data"), True)
-        self.assertEqual(handler.request.responseHeaders.getRawHeaders("Clear-Site-Data")[0], '"*"')
 
 
 class TestTokenAuth(helpers.TestHandlerWithPopulatedDB):
