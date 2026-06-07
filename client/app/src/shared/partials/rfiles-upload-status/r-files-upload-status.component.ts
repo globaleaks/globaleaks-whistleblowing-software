e3d671688da41c9156e573ca9be107dff41eac1d
Fischer86 <fischer8@tutanota.com>
2026-06-07 19:19:48 +0200

    Fix: Estimated upload time format
    Fixes #1638

--- client/app/src/shared/partials/rfiles-upload-status/r-files-upload-status.component.ts
+++ client/app/src/shared/partials/rfiles-upload-status/r-files-upload-status.component.ts
@@ -9,9 +9,22 @@ import {TranslatorPipe} from "@app/shared/pipes/translate";
     standalone: true,
     imports: [NgStyle, TranslateModule, TranslatorPipe]
 })
+
 export class RFilesUploadStatusComponent {
   @Input() uploading: boolean | undefined;
   @Input() progress: number | undefined;
   @Input() estimatedTime: number | undefined;
   protected readonly isFinite = isFinite;
+
+  getFormattedTime(totalSeconds: number | undefined) {
+    if (totalSeconds === undefined || !Number.isFinite(totalSeconds)) {
+      return null;
+    }
+    const days = Math.floor(totalSeconds / 86400);
+    const hours = Math.floor((totalSeconds % 86400) / 3600);
+    const minutes = Math.floor((totalSeconds % 3600) / 60);
+    const seconds = Math.floor(totalSeconds % 60);
+
+    return { days, hours, minutes, seconds};
+  }
 }
