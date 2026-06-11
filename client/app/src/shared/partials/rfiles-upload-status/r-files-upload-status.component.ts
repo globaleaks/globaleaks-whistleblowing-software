4f565ca7dc3f965ae17c0fb417129ad523d7b0fe
Fischer86 <fischer8@tutanota.com>
2026-06-11 20:20:33 +0200

    Changed the time display and fixed a translate pipe conflict

--- client/app/src/shared/partials/rfiles-upload-status/r-files-upload-status.component.ts
+++ client/app/src/shared/partials/rfiles-upload-status/r-files-upload-status.component.ts
@@ -1,30 +1,44 @@
 import {Component, Input} from "@angular/core";
 import {NgStyle} from "@angular/common";
 import {TranslateModule} from "@ngx-translate/core";
-import {TranslatorPipe} from "@app/shared/pipes/translate";
+// import {TranslatorPipe} from "@app/shared/pipes/translate";
+
+type EstimatedTime = {
+  value: number;
+  unit: "seconds" | "minutes" | "hours" | "days";
+};
 
 @Component({
     selector: "src-rfiles-upload-status",
     templateUrl: "./r-files-upload-status.component.html",
     standalone: true,
-    imports: [NgStyle, TranslateModule, TranslatorPipe]
+    imports: [NgStyle, TranslateModule/*, TranslatorPipe*/]
 })
 
 export class RFilesUploadStatusComponent {
   @Input() uploading: boolean | undefined;
   @Input() progress: number | undefined;
   @Input() estimatedTime: number | undefined;
+
   protected readonly isFinite = isFinite;
 
-  getFormattedTime(totalSeconds: number | undefined) {
-    if (totalSeconds === undefined || !Number.isFinite(totalSeconds)) {
+  getFormattedTime(totalSeconds: number | undefined): EstimatedTime | null {
+    if (totalSeconds === undefined || !Number.isFinite(totalSeconds) || totalSeconds < 0) {
       return null;
     }
-    const days = Math.floor(totalSeconds / 86400);
-    const hours = Math.floor((totalSeconds % 86400) / 3600);
-    const minutes = Math.floor((totalSeconds % 3600) / 60);
-    const seconds = Math.floor(totalSeconds % 60);
+    
+    if (totalSeconds >= 86400) {
+      return { value: Math.floor(totalSeconds / 86400), unit: 'days'};
+    }
 
-    return { days, hours, minutes, seconds};
+    if (totalSeconds >= 3600) {
+      return { value: Math.floor(totalSeconds / 3600), unit: 'hours'};
+    }
+
+    if (totalSeconds >= 60) {
+      return { value: Math.floor(totalSeconds / 60), unit: 'minutes'};
+    }
+
+    return { value: Math.floor(totalSeconds), unit: "seconds"};
   }
 }
