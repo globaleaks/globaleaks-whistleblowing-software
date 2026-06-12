82e49ba8ccceac195cf6053df40d75500f47ac04
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2026-06-12 14:48:15 +0200

    Bound the length of the support request text

--- backend/globaleaks/handlers/support.py
+++ backend/globaleaks/handlers/support.py
@@ -1,8 +1,12 @@
 # Handlers dealing with user support requests
 from globaleaks.handlers.base import BaseHandler
-from globaleaks.rest import requests
+from globaleaks.rest import errors, requests
 from globaleaks.utils.log import log
 from globaleaks.utils.defang import defang
+
+# Maximum accepted length of the support request text. The request body is not
+# otherwise bounded and its content is persisted verbatim as queued mail bodies.
+SUPPORT_TEXT_MAX_LENGTH = 4096
 
 def generate_support_email(mail_address, hostname, request):
     email = "From: %s\n\n" % mail_address         # regexp-validated
@@ -20,6 +24,9 @@ class SupportHandler(BaseHandler):
     def post(self):
         request = self.validate_request(self.request.content.read(),
                                         requests.SupportDesc)
+
+        if len(request['text']) > SUPPORT_TEXT_MAX_LENGTH:
+            raise errors.InputValidationError("Support request text too long")
 
         email = generate_support_email(request['mail_address'],
                                        self.request.hostname,
