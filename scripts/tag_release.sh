c7d2562c61ee6abd826a10007af8b525e49c55c1
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2026-06-12 22:12:59 +0200

    Validate the version format in the release scripts

--- scripts/tag_release.sh
+++ scripts/tag_release.sh
@@ -2,11 +2,11 @@
 # This script tag a new release version
 set -e
 
-if [[ "$1" != "" ]]; then
+if [[ "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
 	echo "Tagging version v$1"
 
-        git tag -s v$1 -m 'GlobaLeaks version $1' --force
+        git tag -s "v$1" -m "GlobaLeaks version $1" --force
 else
-	echo -e "Please specify a version"
+	echo -e "Please specify a valid version (expected format: X.Y.Z)"
 	exit 1
 fi
