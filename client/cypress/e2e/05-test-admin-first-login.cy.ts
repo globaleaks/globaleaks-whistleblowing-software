5f9ed6145f6c2ea53949da8984c96d9a1f6b0911
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2026-06-18 17:06:39 +0200

    Update tests in relation to latest development

--- client/cypress/e2e/05-test-admin-first-login.cy.ts
+++ client/cypress/e2e/05-test-admin-first-login.cy.ts
@@ -7,6 +7,18 @@ describe("admin login", () => {
     cy.get("[name='secret']").type(Cypress.env("user_password"));
     cy.get("#confirm").click();
     cy.get('src-encryption-recovery-key').should('exist');
+
+    // The recovery key is shown but hidden by default.
+    cy.get('#AccountRecoveryKey').should('be.visible').and('have.attr', 'type', 'password');
+    cy.takeScreenshot("admin/recoverykey", ".modal-dialog");
+
+    // The show/hide toggle reveals and re-hides the recovery key.
+    cy.get('#toggle-visibility').click();
+    cy.get('#AccountRecoveryKey').should('have.attr', 'type', 'text');
+    cy.takeScreenshot("admin/recoverykey_revealed", ".modal-dialog");
+    cy.get('#toggle-visibility').click();
+    cy.get('#AccountRecoveryKey').should('have.attr', 'type', 'password');
+
     cy.get("#close").click();
 
     cy.logout();
