fa02a02f266c69ccb1e2b6896a864c037b9d72d3
Giovanni Pellerano <giovanni.pellerano@globaleaks.org>
2026-06-28 22:05:00 +0200

    Allow tagging docker releases (X.Y.Z-docker) in tag_release script

--- scripts/tag_release.sh
+++ scripts/tag_release.sh
@@ -2,11 +2,11 @@
 # This script tag a new release version
 set -e
 
-if [[ "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+if [[ "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+(-docker)?$ ]]; then
 	echo "Tagging version v$1"
 
         git tag -s "v$1" -m "GlobaLeaks version $1" --force
 else
-	echo -e "Please specify a valid version (expected format: X.Y.Z)"
+	echo -e "Please specify a valid version (expected format: X.Y.Z or X.Y.Z-docker)"
 	exit 1
 fi
